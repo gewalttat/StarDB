cd37a3f68a91f93722cc8b5ce01b6f93b69d0c51
gewalttat <rbhorses@mail.ru>
2019-10-03 08:26:03 +0300

    create an application framework. written a function getResource(), on the basis of which all other functionality will work.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class SwapiServices {
+    //указание источника данных для getResource
+    apiBase = 'https://swapi.co/api';
+    //основная асинхронная функция, возвращающая промис возвращающий данные с swapi.co(?)
+    async getResource(url){
+        //res = ответ сервера
+        const res = await fetch(`${this.apiBase}${url}`);
+        //бросает ошибку если нет респонса
+        if (!res.ok) {
+            throw new Error()
+        }
+        //отдает респонс в жсыне
+        return await res.json();
+    };
+    //отдают респонс или ошибку по разделам swapi.co
+    //в теле функции идут только разделы т.к. основной адрес получения указан в apiBase
+    getAllPeople() {
+        return this.getResource(`/people/`);
+    };
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`);
+    };
+}
