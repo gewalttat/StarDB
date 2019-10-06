736dfd145ecee880dcc298c0c77df8b0c08d36b1
gewalttat <rbhorses@mail.ru>
2019-10-06 10:57:11 +0300

    заполнение основных файлов. что-то не работает -_-

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,28 @@
 import React from 'react';
+
 import './header.css';
-//заголовок, ничего интересного
+
 const Header = () => {
-    return (
-        //присваивание заголовкам классов, прописывание заголовков 
-    <div className='header d-flex'>
-    <h1><a href='#'>Star Wars Database</a></h1>
-    <ul className='asd'>
-    <li><a href='#'>People</a></li>
-    <li><a href='#'>Planets</a></li>
-    <li><a href='#'>Starships</a></li>
-    </ul>
+  return (
+    <div className="header d-flex">
+      <h3>
+        <a href="#">
+          StarDB
+        </a>
+      </h3>
+      <ul className="d-flex">
+        <li>
+          <a href="#">People</a>
+        </li>
+        <li>
+          <a href="#">Planets</a>
+        </li>
+        <li>
+<a href="#">Starships</a>
+        </li>
+      </ul>
     </div>
-    );
-    };
-    export default Header;
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,6 @@
-class SwapiService {
-    //указание источника данных для getResource
-    _apiBase = 'https://swapi.co/api';
-    //основная асинхронная функция, возвращающая промис возвращающий данные с swapi.co(?)
-    async getResource(url) {
-        //res = ответ сервера
-        const res = await fetch(`${this.apiBase}${url}`);
-        //бросает ошибку если нет респонса
-        if (!res.ok) {
-            throw new Error()
-        };
-        //отдает респонс в жсыне
-        return await res.json();
-    };
-    //отдают респонс или ошибку по разделам swapi.co
-    //в теле функции идут только разделы т.к. основной адрес получения указан в apiBase
-    async getAllPeople() {
-        const res = await this.getResource(`/people/`);
-        //возвращает респонс массивом
-        return res.results;
-    };
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`);
-    };
-    async getAllPlanets() {
-        const res = await this.getResource(`/planets/`);
-        return res.results;
-    };
-    getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
-    };
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-    async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-    };
-
-    getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
-    };
-};
-//swapi получает функцию класса с промисом
-const swapi = new SwapiService();
-swapi.getPerson(3).then((p) => {
-    console.log(p.name);
-});
+ReactDOM.render(<App/>,
+  document.getElementById('root'));
