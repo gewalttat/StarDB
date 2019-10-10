58b5b419a4f1f1c35fe7bcc6e74db38bb4cb800f
gewalttat <rbhorses@mail.ru>
2019-10-10 23:09:51 +0300

    work on getting data from swapi.co into my swapiService. added "Converter" _extractId() as a regular expression getting the last numbers of the url string inside swapi.co.

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -26,9 +26,10 @@ export default class SwapiService {
         const res = await this.getResource(`/planets/`);
         return res.results;
     };
-    getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
-    };
+    async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
+};
 
     async getAllStarships() {
     const res = await this.getResource(`/starships/`);
@@ -38,4 +39,42 @@ export default class SwapiService {
     getStarship(id) {
     return this.getResource(`/starships/${id}/`);
     };
+    
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    };
+    
+    _transformPlanet(planet) {
+        return  { 
+            id: this._extractId(planet),
+        name: planet.name,
+        population: planet.population,
+        diameter: planet.diameter,
+        rotationPeriod: planet.rotation_period
+    }
 };
+_transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    }
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    }
+  }
+}
