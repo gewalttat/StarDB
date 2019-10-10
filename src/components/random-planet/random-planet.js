5600e9e21b76c5df0d19897b00dc852d8212186e
gewalttat <rbhorses@mail.ru>
2019-10-10 23:36:20 +0300

    changed state: now state is an empty object "planet", which in turn by destructurization takes state components from swapiService to onplanetloaded function; then this function is passed to updatePlanet.

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -5,11 +5,7 @@ export default class RandomPlanet extends Component {
 
   swapiService = new SwapiService();
   state = {
-    id:null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter:null
+    planet: {}
   };
 
   constructor() {
@@ -25,21 +21,13 @@ export default class RandomPlanet extends Component {
     const id = Math.floor(Math.random()*25)+2;
     this.swapiService
       .getPlanet(id)
-      .then((planet) => {
-        this.setState({
-          id,
-name: planet.name,
-population: planet.population,
-diameter: planet.diameter,
-rotationPeriod: planet.rotation_period
-        });
-      });
+      .then(this.onPlanetLoaded);
   }
 
   render() {
 
-    const { id, name, population,
-      rotationPeriod, diameter }  = this.state;
+    const { planet: { id, name, population,
+      rotationPeriod, diameter } } = this.state;
 
     return (
       <div className="random-planet jumbotron rounded">
