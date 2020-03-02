e2734aeb26b6b391db492466917178be65536db5
gewalttat <rbhorses@mail.ru>
2020-03-02 21:49:41 +0300

    КАМБЕК С ФОНТАНА

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,67 +1,59 @@
 import React, { Component } from 'react';
-
-
-
 import './item-list.css';
 import SwapiService from "../../services/swapi-service";
 import Preloader from "../preloader/preloader";
 
-export default class ItemList extends Component {
-
-  state = {
-    itemList: null
-  };
-
-  componentDidMount() {
-
-    const { getData } = this.props;
-//запрос на данные 
-    getData()
-      .then((itemList) => {
-        //обновление стейта айтемлист полученными данными
-        this.setState({
-          itemList
-        });
-      });
-  }
-
-  renderItems(arr) {
-    return arr.map((item) => {
+//механика
+ const ItemList = (props) => {
+    const {data, onItemSelected, children:renderLabel} = props;
+    const items = data.map((item) => {
       const { id } = item;
-      const label = this.props.children(item);
+      const label = renderLabel(item);
 
       return (
         <li className="list-group-item"
             key={id}
-            onClick={() => this.props.onItemSelected(id)}>
+            onClick={() => onItemSelected(id)}>
           {label}
         </li>
       );
     });
-  }
-
-  render() {
-
-    const { itemList } = this.state;
-//отображает лодер если нет данных
-    if (!itemList) {
-      return <Preloader />;
-    }
-
-    const items = this.renderItems(itemList);
-
     return (
       <ul className="item-list list-group">
         {items}
       </ul>
     );
-  }
-}
-
-const f = (a) => {
+  };
+//функция может возвращать другую функцию
+//картинка
+const withData = (View, getData) => {
   return class extends Component  {
+    state = {
+      data : null
+    };
+
+    componentDidMount() {
+  //запрос на данные 
+      getData()
+        .then((data) => {
+          //обновление стейта айтемлист полученными данными
+          this.setState({
+            data
+          });
+        });
+    }
     render() {
-      return <ItemList {...this.props}/>
+      const { data } = this.state;
+//отображает лодер если нет данных
+    if (!data) {
+      return <Preloader />;
+    }
+      //ловит всё что передалось из app в итемлист
+      return <View {...this.props} data={data}/>
     }
   }
 };
+//данные с сервиса
+const {getAllPeople} = new SwapiService();
+//понятия не имею для чего нужно было это разделение дающее независимость
+export default withData(ItemList, getAllPeople);
