afef3d116cd9c8ee61096be9ad76885b5ea55f68
gewalttat <rbhorses@mail.ru>
2020-05-19 23:41:08 +0300

    разные мелкие чистки

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
-
-import ItemDetails, { Record } from "../item-details/item-details";
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
-
 import { SwapiServiceProvider } from '../swapi-service-context';
-
+import Row from '../row';
 import {
   PersonDetails,
   PlanetDetails,
@@ -27,7 +23,7 @@ export default class App extends Component {
 state = {
     showRandomPlanet: true,
     //свапи кидается в стейт чтобы перерисовывать данные по клику
-    swapiService : new DummySwapiService()
+    swapiService : new SwapiService()
   };
 
   //кнопка смены подачи вводных данных (с апи на локальные тестовые данные)
@@ -42,69 +38,20 @@ return {
 });
 };
   
-toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
-
 render() {
   const planet = this.state.showRandomPlanet ?
   <RandomPlanet/> :
   null;
-
-const { getPerson,
-        getStarship,
-        getPersonImage,
-        getStarshipImage } = this.state.swapiService;
-
-const personDetails = (
-  <ItemDetails
-    itemId={11}
-    getData={getPerson}
-    getImageUrl={getPersonImage} >
-
-    <Record field="gender" label="Gender" />
-    <Record field="eyeColor" label="Eye Color" />
-
-  </ItemDetails>
-);
-
-const starshipDetails = (
-  <ItemDetails
-    itemId={5}
-    getData={getStarship}
-    getImageUrl={getStarshipImage}>
-
-    <Record field="model" label="Model" />
-    <Record field="length" label="Length" />
-    <Record field="costInCredits" label="Cost" />
-  </ItemDetails>
-);
-
 return (
   <ErrorBoundry>
   {/* присвоение провайдера всему приложению */}
   <SwapiServiceProvider value={this.state.swapiService}>
     <div className="stardb-app">
       <Header onServiceChange={this.onServiceChange}/>
-
-      {/* по идее кусок уже не нужен, но пусть поваляется зачем нибудь
-      <Row
-      left={personDetails}
-      right={starshipDetails} />
-      */}
-
-      <PersonDetails itemId={1}/>
-      <PlanetDetails itemId={2}/>
-      <StarshipDetails itemId={3}/>
-
-      <PersonList/>
-      <PlanetList/>
-      <StarshipList/>
-
+      {planet}
+      {<Row left={<PersonList/>} right={<PersonDetails itemId={1}/>} />}
+      {<Row left={<PlanetList/>} right={<PlanetDetails itemId={2}/>} />}
+      {<Row left={<StarshipList/>} right={<StarshipDetails itemId={3}/>} />}
     </div>
     </SwapiServiceProvider>
   </ErrorBoundry>
